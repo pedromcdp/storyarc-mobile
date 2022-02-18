a83bce81b1b0fed94dfc99137fc189095e3682a0
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-02-18 17:10:21 +0000

    Added some top padding and centered the elementes (EmptyCommentList)

--- src/components/shared/EmptyCommentList.js
+++ src/components/shared/EmptyCommentList.js
@@ -3,7 +3,7 @@ import { MaterialIcons } from "@expo/vector-icons";
 
 export function EmptyCommentList() {
   return (
-    <VStack space="2">
+    <VStack space="2" pt={10} justifyContent={"center"}>
       <MaterialIcons name="error-outline" size={24} color="black" />
       <Text>Sem Comentários</Text>
     </VStack>
